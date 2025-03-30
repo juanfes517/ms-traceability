6e2180ae9b46bd416e3d297fcb28d223d767eb4a
Juan Felipe Escobar Rendón <juan.escobar15@udea.edu.co>
2025-03-30 18:37:00 -0500

    feat: add security filter to validate the JWT

--- src/main/java/com/pragma/traceability/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/com/pragma/traceability/infrastructure/configuration/SecurityConfiguration.java
@@ -1,4 +1,34 @@
 package com.pragma.traceability.infrastructure.configuration;
 
+import com.pragma.traceability.domain.spi.IJwtSecurityServicePort;
+import com.pragma.traceability.infrastructure.configuration.securityfilter.JwtTokenValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final IJwtSecurityServicePort jwtSecurityServicePort;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.anyRequest().permitAll();
+                })
+                .addFilterBefore(new JwtTokenValidator(jwtSecurityServicePort), BasicAuthenticationFilter.class)
+                .build();
+    }
 }
