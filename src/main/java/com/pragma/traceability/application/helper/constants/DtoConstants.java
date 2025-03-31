e6f6f0c830df20db4c317f41eaeec08bd9ecc442
Juan Felipe Escobar Rendón <juan.escobar15@udea.edu.co>
2025-03-31 09:55:48 -0500

    feat: add field validations when creating a traceability request

---
+++ src/main/java/com/pragma/traceability/application/helper/constants/DtoConstants.java
@@ -0,0 +1,11 @@
+package com.pragma.traceability.application.helper.constants;
+
+public class DtoConstants {
+
+    private DtoConstants() {}
+
+    public static final String ORDER_ID_CANNOT_BE_BLANK = "The order id cannot be blank";
+    public static final String CUSTOMER_ID_CANNOT_BE_BLANK = "The customer id cannot be blank";
+    public static final String CUSTOMER_EMAIL_CANNOT_BE_BLANK = "The customer email cannot be blank";
+    public static final String NEW_STATUS_CANNOT_BE_BLANK = "The new status cannot be blank";
+}
