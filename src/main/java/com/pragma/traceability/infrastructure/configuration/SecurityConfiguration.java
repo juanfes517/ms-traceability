410a31663fd0015a1efe81759d0329dcd49afe72
Juan Felipe Escobar Rendón <juan.escobar15@udea.edu.co>
2025-03-31 10:40:03 -0500

    refactor: delete unused import

--- src/main/java/com/pragma/traceability/infrastructure/configuration/SecurityConfiguration.java
+++ src/main/java/com/pragma/traceability/infrastructure/configuration/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import com.pragma.traceability.infrastructure.helper.constants.SecurityConstants
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
